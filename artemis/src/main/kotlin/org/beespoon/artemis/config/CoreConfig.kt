50c21bb8b27ca6f4e16a84fc0a6dacf1370604ae
Ben Soones <76079263+BSpoones@users.noreply.github.com>
2025-06-21 18:45:24 +0100

    feat: CoreConfig

---
+++ artemis/src/main/kotlin/org/beespoon/artemis/config/CoreConfig.kt
@@ -0,0 +1,36 @@
+package org.beespoon.artemis.config
+
+import net.dv8tion.jda.api.requests.GatewayIntent
+import org.beespoon.artemis.Artemis
+import org.beespoon.artemis.util.config.base.ActionableConfig
+import org.spongepowered.configurate.objectmapping.ConfigSerializable
+import java.nio.file.WatchEvent
+import kotlin.system.exitProcess
+
+/**
+ * Main Zeus Config file
+ *
+ * This file contains all setup configuration
+ */
+@ConfigSerializable
+internal class CoreConfig : ActionableConfig("core") {
+    var token: String = ""
+
+    var globalMessagePrefix: String = "!"
+
+    var whitelistedGuildIds: MutableList<Long> = mutableListOf()
+
+    var guildPrefixMap: MutableMap<Long, String> = mutableMapOf()
+
+    var gatewayIntents: MutableList<GatewayIntent> = GatewayIntent.entries.toMutableList()
+
+    // Any edits to this file should require a restart
+    override fun onChange(event: WatchEvent<*>) {
+        Artemis.logger.warn("Core Config has been changed! Restarting in:")
+        for (i in 5 downTo 1) {
+            println(i)
+            Thread.sleep(1_000)
+        }
+        exitProcess(1)
+    }
+}
