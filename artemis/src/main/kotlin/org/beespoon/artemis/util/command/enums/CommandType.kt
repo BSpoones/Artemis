6f2e3d77c845445190204aff33bbff7bc3878428
Ben Soones <76079263+BSpoones@users.noreply.github.com>
2025-06-21 18:49:54 +0100

    Create CommandType.kt

---
+++ artemis/src/main/kotlin/org/beespoon/artemis/util/command/enums/CommandType.kt
@@ -0,0 +1,36 @@
+package org.beespoon.artemis.util.command.enums
+
+import net.dv8tion.jda.api.interactions.commands.Command
+
+/**
+ * Command Type Enum
+ * 
+ * Used to help locate commands on the command tree
+ * 
+ * @see org.beespoon.artemis.util.command.tree
+ * @author <a href="https://www.bspoones.com">BSpoones</a>
+ */
+enum class CommandType {
+    SLASH,
+    MESSAGE,
+    USER_CONTEXT,
+    MESSAGE_CONTEXT;
+
+    fun toDiscord(): Command.Type {
+        return when (this) {
+             SLASH -> Command.Type.SLASH
+             MESSAGE -> Command.Type.UNKNOWN
+             USER_CONTEXT -> Command.Type.USER
+             MESSAGE_CONTEXT -> Command.Type.MESSAGE
+        }
+    }
+}
+
+internal fun Command.Type.toCommandType(): CommandType {
+    return when (this) {
+        Command.Type.SLASH -> CommandType.SLASH
+        Command.Type.UNKNOWN -> CommandType.MESSAGE
+        Command.Type.USER -> CommandType.USER_CONTEXT
+        Command.Type.MESSAGE -> CommandType.MESSAGE_CONTEXT
+    }
+}
