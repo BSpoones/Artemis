5b1a09c1259372eb7316fe4e5efefba6b84860a8
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-06-30 20:30:15 +0100

    init

--- artemis/src/main/kotlin/org/beespoon/artemis/command/registry/CommandRegistry.kt
+++ artemis/src/main/kotlin/org/beespoon/artemis/command/registry/CommandRegistry.kt
@@ -1,4 +1,10 @@
 package org.beespoon.artemis.command.registry
 
+import org.beespoon.artemis.command.Command
+
 object CommandRegistry {
+
+    fun <T: Command> registerCommands(clazz: T) {
+
+    }
 }
