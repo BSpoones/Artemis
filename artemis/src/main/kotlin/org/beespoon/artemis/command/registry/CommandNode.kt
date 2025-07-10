fdcaae2398a089b0880778fdc95da1ea0205e523
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-07-10 23:39:22 +0100

    fix: getMessageOptions

--- artemis/src/main/kotlin/org/beespoon/artemis/command/registry/CommandNode.kt
+++ artemis/src/main/kotlin/org/beespoon/artemis/command/registry/CommandNode.kt
@@ -36,58 +36,27 @@ data class CommandNode(
 
     }
 
-
     fun getMessageOptions(arg: String, type: KType, attachment: Attachment? = null): Any? {
+        val api = JdaApi.api()
+        val id = arg.numbersOnly()?.toLongOrNull()
         return when (type.classifier) {
+            Attachment::class.java -> attachment
             String::class -> arg
             Int::class -> arg.toIntOrNull()
             Long::class -> arg.toIntOrNull()
             Double::class -> arg.toDoubleOrNull()
             Boolean::class -> arg.toBooleanStrictOrNull()
-            User::class.java -> {
-                if (arg.isUserPing()) {
-                    val id = arg.numbersOnly() ?: return null
-                    // TODO -> API Access
-//                    CommandRegistry.api.getUserById(id.toLong())
-                    null
-                } else {
-                    null
-                }
+            User::class.java -> if (arg.isUserPing() && id != null) api.getUserById(id) else null
+            Role::class.java -> if (arg.isRolePing() && id != null) api.getRoleById(id) else null
+            Channel::class.java -> if (arg.isChannelPing() && id != null)
+                api.getChannelById((type as Channel)::class.java, id) else null
+
+            IMentionable::class.java -> when {
+                arg.isRolePing() && id != null -> api.getRoleById(id)
+                arg.isUserPing() && id != null -> api.getUserById(id)
+                else -> null
             }
-            Channel::class.java -> {
-                if (arg.isChannelPing()) {
-                    val id = arg.numbersOnly() ?: return null
-                    // TODO -> API Access
-//                    CommandRegistry.api.getChannelById((parameterType as Channel)::class.java, id.toLong())
-                    null
-                } else {
-                    null
-                }
-            }
-            Role::class.java -> {
-                if (arg.isRolePing()) {
-                    val id = arg.numbersOnly() ?: return null
-                    // TODO -> API Access
-//                    CommandRegistry.api.getRoleById(id.toLong())
-                    null
-                } else {
-                    null
-                }
-            }
-            IMentionable::class.java -> {
-                val roleMatch = ROLE_PING_REGEX.matches(arg)
-                val userMatch = USER_PING_REGEX.matches(arg)
-                if (!arg.isRolePing() && !arg.isUserPing()) null
-                else {
-                    val id = arg.numbersOnly() ?: return null
-                    // TODO -> API Access
-//                    if (roleMatch) CommandRegistry.api.getRoleById(id.toLong())
-//                    else  CommandRegistry.api.getUserById(id.toLong())
-                }
-            }
-            Attachment::class.java -> {
-                attachment
-            }
+
             else -> null // Handle other types as needed
         }
     }
