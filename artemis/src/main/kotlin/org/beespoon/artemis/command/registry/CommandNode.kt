45a126ef0a83fa6a2e43d88aaaeaf9a1131dfd43
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-07-14 23:06:44 +0100

    refactor

--- artemis/src/main/kotlin/org/beespoon/artemis/command/registry/CommandNode.kt
+++ artemis/src/main/kotlin/org/beespoon/artemis/command/registry/CommandNode.kt
@@ -1,16 +1,11 @@
 package org.beespoon.artemis.command.registry
 
-import net.dv8tion.jda.api.entities.IMentionable
-import net.dv8tion.jda.api.entities.Message.Attachment
-import net.dv8tion.jda.api.entities.Role
-import net.dv8tion.jda.api.entities.User
-import net.dv8tion.jda.api.entities.channel.Channel
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import org.beespoon.artemis.command.annotation.command.CommandOption
-import org.beespoon.artemis.util.extensions.*
 import org.beespoon.artemis.util.extensions.getOptionValue
 import kotlin.reflect.KFunction
-import kotlin.reflect.KType
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.hasAnnotation
 
@@ -20,8 +15,10 @@ data class CommandNode(
     val description: String?,
     val usage: String?,
     val method: KFunction<*>,
+    val command: CommandData,
     val children: MutableList<CommandNode>
 ) {
+
     fun getSlashArgs(event: SlashCommandInteractionEvent): Array<Any?> {
         return method.parameters
             .filter { it.hasAnnotation<CommandOption>() }
@@ -33,31 +30,7 @@ data class CommandNode(
     }
 
     fun getMessageArgs(command: String): Array<Any?> {
-
-    }
-
-    fun getMessageOptions(arg: String, type: KType, attachment: Attachment? = null): Any? {
-        val api = JdaApi.api()
-        val id = arg.numbersOnly()?.toLongOrNull()
-        return when (type.classifier) {
-            Attachment::class.java -> attachment
-            String::class -> arg
-            Int::class -> arg.toIntOrNull()
-            Long::class -> arg.toIntOrNull()
-            Double::class -> arg.toDoubleOrNull()
-            Boolean::class -> arg.toBooleanStrictOrNull()
-            User::class.java -> if (arg.isUserPing() && id != null) api.getUserById(id) else null
-            Role::class.java -> if (arg.isRolePing() && id != null) api.getRoleById(id) else null
-            Channel::class.java -> if (arg.isChannelPing() && id != null)
-                api.getChannelById((type as Channel)::class.java, id) else null
-
-            IMentionable::class.java -> when {
-                arg.isRolePing() && id != null -> api.getRoleById(id)
-                arg.isUserPing() && id != null -> api.getUserById(id)
-                else -> null
-            }
-
-            else -> null // Handle other types as needed
-        }
+        // TODO
+        return arrayOf()
     }
 }
