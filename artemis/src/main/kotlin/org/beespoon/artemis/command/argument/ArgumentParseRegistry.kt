8f7ccb74a87fcaad70035dc4caf84e91978c0aec
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-07-16 16:44:51 +0100

    add: suggestion handling

--- artemis/src/main/kotlin/org/beespoon/artemis/command/argument/ArgumentParseRegistry.kt
+++ artemis/src/main/kotlin/org/beespoon/artemis/command/argument/ArgumentParseRegistry.kt
@@ -1,5 +1,10 @@
 package org.beespoon.artemis.command.argument
 
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
+import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent
+import java.util.concurrent.CompletableFuture
+import kotlin.reflect.KClass
+
 object ArgumentParseRegistry {
 
     private val parsers = mutableMapOf<KClass<*>, ArgumentParser<*, *>>()
@@ -26,5 +31,20 @@ object ArgumentParseRegistry {
 
     inline fun <reified Input : Any, reified Result : Any> parse(event: GenericCommandInteractionEvent): ArgumentParseResult<Result>? {
         return parse(Input::class, event)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <Result : Any> suggestions(
+        clazz: KClass<Result>,
+        event: CommandAutoCompleteInteractionEvent
+    ): CompletableFuture<List<String>> {
+        val current = event.focusedOption.value
+        val parser = parsers[clazz] as? ArgumentParser<String, Result>
+            ?: return CompletableFuture.completedFuture(emptyList())
+        return parser.suggestions(event, current)
+    }
+
+    inline fun <reified Result : Any> suggestions(event: CommandAutoCompleteInteractionEvent): CompletableFuture<List<String>> {
+        return suggestions(Result::class, event)
     }
 }
