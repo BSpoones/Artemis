9a5cc4a4ec270759e9a9b2d87aecab267414f5cd
BSpoones <76079263+BSpoones@users.noreply.github.com>
2025-07-19 23:11:13 +0100

    add: Pass/Fail subclass

--- artemis/src/main/kotlin/org/beespoon/artemis/command/argument/ArgumentParseResult.kt
+++ artemis/src/main/kotlin/org/beespoon/artemis/command/argument/ArgumentParseResult.kt
@@ -1,10 +1,15 @@
 package org.beespoon.artemis.command.argument
 
-class ArgumentParseResult<T> {
+sealed class ArgumentParseResult<out A> {
+    data class Pass<out A>(val value: A) : ArgumentParseResult<A>()
+    data class Fail(val error: String) : ArgumentParseResult<Nothing>()
 
-    fun pass(value: T) = value
+    fun isPass(): Boolean = this is Pass
 
-    fun fail(reason: String) {
+    fun getOrNull(): A? = (this as? Pass)?.value
 
+    companion object {
+        fun <A> success(value: A): ArgumentParseResult<A> = Pass(value)
+        fun failure(error: String): ArgumentParseResult<Nothing> = Fail(error)
     }
 }
